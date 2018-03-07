aef8d11079b6de02271ec281e6ec9c5bd8aaa800
Chirag Vashist <chiragvashist007@gmail.com>
2018-03-08 00:31:42 +0530

    Added Italian and Dutch Words

--- languagescripts/es.py
+++ languagescripts/es.py
@@ -1,10 +1,10 @@
-f = open("cs.txt","r")
+f = open("it.txt","r")
 naam = open("new.txt","w")
 a = ""
 b = f.readline()
 while(b):
-    g = b.split(" ")[0]
-    p = g.strip(" ")
+    g = b.split(" ")[1]
+    p = g.strip("\n")
     naam.write(p)
     naam.write(",")
     naam.write("\n")
